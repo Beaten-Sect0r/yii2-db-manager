2d16af681e3bb82b7cabae4500446a053cd6a858
Alexander Volkov <the.purgen@gmail.com>
2019-02-06 17:49:52 +0300

    fix DefaultController::prepareFileData()

--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -267,7 +267,6 @@ class DefaultController extends Controller
      */
     protected function prepareFileData()
     {
-        $dataArray = [];
         foreach ($this->getModule()->getFileList() as $id => $file) {
             $columns = [];
             $columns['id'] = $id;
@@ -275,6 +274,7 @@ class DefaultController extends Controller
             $columns['name'] = StringHelper::basename($file);
             $columns['size'] = Yii::$app->formatter->asSize(filesize($file));
             $columns['create_at'] = Yii::$app->formatter->asDatetime(filectime($file));
+            $dataArray = [];
             $dataArray[] = $columns;
         }
         ArrayHelper::multisort($dataArray, ['create_at'], [SORT_DESC]);
